8e4ae0f4547a17998eb86f9aebf21fb4ca5f4953
jingyiwang <jingyiwang@jingyis-mbp.myfiosgateway.com>
2021-03-26 13:31:32 -0400

    configured AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "jingyiwang-notes-app-upload",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://24lvwvq5ij.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_bnpkbQCjc",
+    APP_CLIENT_ID: "56db18qafg2r4qcbesn7i4qcvu",
+    IDENTITY_POOL_ID: "us-east-1:12451eb6-d10d-4d0f-bb4b-e6e51b469ef0",
+  },
+};
+
+export default config;
