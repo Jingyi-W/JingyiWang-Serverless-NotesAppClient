87720be8b0b3050c94d7c177338f7922a8a9908a
jingyiwang <jingyiwang@jingyis-mbp.myfiosgateway.com>
2021-03-26 21:51:28 -0400

    added the settings page for stripe payment information

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
   MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51IUbiVDrFuHCRg6JVKQog1h3dT0LbLOZo8PLnchbiKxNVmlgthQq8RJi2Xv4yg4i3c2aqFj0kIXVmh9ZO6sfn9ja00WTWdXG28",
   s3: {
     REGION: "us-east-1",
     BUCKET: "jingyiwang-notes-app-upload",
